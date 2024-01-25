281964ba40106c53a73521a01bd469021bff368c
Shivali Deepak Patil <90774044+shivalipatil22@users.noreply.github.com>
2024-01-25 17:47:10 +0530

    Add files via upload

---
+++ CrudGetPostApplication.java
@@ -0,0 +1,13 @@
+package com.hefshine.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudGetPostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudGetPostApplication.class, args);
+	}
+
+}
